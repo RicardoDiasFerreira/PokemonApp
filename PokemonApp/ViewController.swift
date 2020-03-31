6b453a30c0521a6ad23b88ffdbf4406864b18fc5
Ricardo Ferreira <ricardodiasferreira10@gmail.com>
2020-03-31 23:22:02 +0100

    first commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Ricardo Ferreira on 31/03/2020.
+//  Copyright © 2020 Ricardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PokemonAppTests/PokemonAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  PokemonAppTests.swift
+//  PokemonAppTests
+//
+//  Created by Ricardo Ferreira on 31/03/2020.
+//  Copyright © 2020 Ricardo Ferreira. All rights reserved.
+//
+
+import XCTest
+@testable import PokemonApp
+
+class PokemonAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
